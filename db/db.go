00476b11a1aaa2dbbc27befc4bfb9962d9e8abc3
maalroja <maalroja@bancolombia.com.co>
2023-09-15 18:47:40 -0300

    chore: add database Redis operations and Controllers

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"errors"
+
+	"github.com/go-redis/redis"
+)
+
+type Database struct {
+	Client *redis.Client
+}
+
+var (
+	ErrNil = errors.New("no matching record found in redis database")
+	Ctx    = context.TODO()
+)
+
+func NewDatabase(address string) (*Database, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: "",
+		DB:       0,
+	})
+
+	if err := client.Ping().Err(); err != nil {
+		return nil, err
+	}
+	return &Database{
+		Client: client,
+	}, nil
+}
--- main.go
+++ main.go
@@ -1,22 +1,81 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/go-redis/redis"
+	"github.com/gin-gonic/gin"
+	"github.com/mrojasb2000/go-redis-app/db"
+)
+
+var (
+	ListenAddr = "localhost:8080"
+	RedisAddr  = "localhost:6379"
 )
 
 func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+	database, err := db.NewDatabase(RedisAddr)
+	if err != nil {
+		log.Fatalf("Failed to connect to redis: %s", err.Error())
+	}
+	router := initRouter(database)
+	router.Run(ListenAddr)
+}
+
+func initRouter(database *db.Database) *gin.Engine {
+	r := gin.Default()
+	r.POST("/points", func(c *gin.Context) {
+		var userJson db.User
+		if err := c.ShouldBindJSON(&userJson); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		err := database.SaveUser(&userJson)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"user": userJson,
+		})
 	})
 
-	client.Set("language", "Go", 0)
-	language := client.Get("language")
-	year := client.Get("year")
+	r.GET("/points/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		user, err := database.GetUser(username)
+		if err != nil {
+			if err == db.ErrNil {
+				c.JSON(http.StatusNotFound, gin.H{
+					"error": "No record found for " + username,
+				})
+				return
+			}
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"user": user,
+		})
+	})
 
-	fmt.Println(language.Val())
-	fmt.Println(year.Val())
+	r.GET("/leaderboard", func(c *gin.Context) {
+		leaderboard, err := database.GetLeaderBoard()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"leaderboard": leaderboard,
+		})
+	})
+
+	return r
 }
